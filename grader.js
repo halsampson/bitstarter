4addd52d282f5314f2f0d061df46cad83700e70e
Ubuntu <ubuntu@ip-172-31-37-136.us-west-2.compute.internal>
2013-07-07 01:27:38 +0000

    Grader URL

--- grader.js
+++ grader.js
@@ -24,17 +24,47 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
+        console.error("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
 };
+
+
+var parseResponse = function(result, response) {
+    if (result instanceof Error) {
+	console.error('URL error: ' + util.format(result.message));
+        process.exit(2);
+    } else { // OK
+        program.file = "FromURL.html";
+        fs.writeFileSync(program.file, result);
+        checkHtml();
+    }
+}
+
+
+var checkHtml = function(){
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
+
+var assertUrlExists = function(inUrl) {
+    program.file = "";
+    rest.get(inUrl).on('complete', parseResponse);
+    return inUrl;
+}
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,10 +95,11 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html URL>', 'URL to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.file.length) // no wait for URL response
+      checkHtml();
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
